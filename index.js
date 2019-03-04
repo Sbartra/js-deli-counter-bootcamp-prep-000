5f1d95650980c3e0f33b45651d2f9921dd326c02
Segundo Bartra <segundobartra@gmail.com>
2019-03-04 22:20:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,54 +1,33 @@
-katz_deli = ["Moshe", "Fayge", "Rivki"]
+func stringForDeliLine(deliLine: [String]) -> String {
+    
+    if deliLine.count == 0 {
+        return "The line is currently empty."
+    }
+    
+    var result = "The line is:"
+    for var i = 0; i < deliLine.count; i++ {
+        let numberInLine = i + 1
+        result += "\n\(numberInLine). \(deliLine[i])"
+    }
+    print(result)
+    return result
+}
+    
+func addName(name: String, toDeliLine deliLine: [String]) -> [String] {
+    var newDeliLine = deliLine
+    newDeliLine.append(name)
+    print("Welcome \(name)! You are number \(newDeliLine.count) in line.")
+    return newDeliLine
+}
+        
+func serveNextCustomerInDeliLine(deliLine: [String]) -> [String] {
+    var newDeliLine = deliLine
 
-
-def line(array) # this was the one I figured out
-  if array.length >= 1
-    nuarray = []
-    counter = 1
-    array.each do |name|
-      nuarray.push("#{counter}. #{name}")
-      counter += 1
-    end
-    puts "The line is currently: #{nuarray.join(" ")}"
-  else
-    puts "The line is currently empty."
-  end
-end
-
-line(katz_deli)
-
-def line_simple(array) # this one follows the same mechanics as learn.co
-  current_line = "The simple line is currently:"
-  array.each.with_index(1) do |value, indexemus|
-  # "each.with_index" is the method...must use "index"
-    current_line << " #{indexemus}. #{value},"
-    # "indexemus" is used to illustrate this variable can be different from "index"
-  end
-  puts current_line
-end
-
-line_simple(katz_deli)
-#[:foo, :bar, :baz].each.with_index(1) do |value, index|
-#    puts "#{index}: #{value}"
-#end
-
-
-
-def take_a_number(line, new_person)
-  line.push(new_person) # could say: "line << new_person"
-  puts "Welcome, #{new_person}. You are number #{line.length} in line."
-end
-
-take_a_number(katz_deli, "Fyvish")
-
-def now_serving(line)
-  if line.length == 0 # could say: "if deli.empty?"
-    puts"There is nobody waiting to be served!"
-  else
-    puts "Currently serving #{line[0]}." # could say: "Currently serving #{line.first}."
-    line.shift # this works in the IDE but no on repl.it
-  end
-end
-
-puts now_serving(katz_deli)
-puts katz_deli
+    if newDeliLine.count > 0 {
+        let nextCustomer = newDeliLine.removeFirst()
+        print("Now serving \(nextCustomer)")
+    } else {
+        print("The line is currently empty.")
+    }
+    return newDeliLine
+}
