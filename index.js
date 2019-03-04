818db4276f4d4c5d51bb4be2528862a1cc2f276f
Segundo Bartra <segundobartra@gmail.com>
2019-03-04 22:10:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,27 +1,41 @@
-var katzDeliLine = [];
-function takeANumber(katzDeli, name) {
-  katzDeli.push(`${name}`);
-  return(`Welcome, ${name}. You are number ${katzDeli.length} in line.`);
-}
-function nowServing(katzDeli) {
-  let i = 0;
-  while (i < katzDeli.length) {
-    i++;
-  }
-  if (katzDeli.length === 0) {
-    return "There is nobody waiting to be served!";
-  }
-  else
-  return (`Currently serving ${katzDeli.shift()}.`);
-}
-var line = [];
+var katzDeli = [];
 
- function currentLine(katzDeli) {
-  for (let i = 0; i < katzDeli.length; i++) {
-    line.push(` `+[i+1]+`. `  + katzDeli[i])
-  }
-  if (katzDeli.length === 0) {
+function currentLine(line){
+  if(!line.length) {
     return "The line is currently empty.";
-  } else
-  return(`The line is currently:` + line);
+  }
+  var lineNamesandNumbers = [];
   
+  for(var i=0; i<line.length; i++) {
+    lineNamesandNumbers.push(i+1 + ". "+ line[i]);
+  }
+  console.log("The line is currently: " + lineNamesandNumbers)
+  return "The line is currently: " + lineNamesandNumbers.join(', ');
+}
+
+function nowServing(line) {
+  if(!line.length) {
+    console.log("There is nobody waiting to be served!")
+    return "There is nobody waiting to be served!"
+  } else {
+    //console.log("Currently serving " + line.shift());
+    return "Currently serving " + line.shift();
+  }
+}
+
+function takeANumber(line, name){
+  line.push(name);
+  
+  console.log("Welcome, " + name + ". You are number " + line.length + " in line.");
+  
+  return "Welcome, " + name + ". You are number " + line.length + " in line."
+}
+takeANumber(katzDeli, "Ada")
+takeANumber(katzDeli, "Grace")
+takeANumber(katzDeli, "Kent")
+currentLine(katzDeli);
+nowServing(katzDeli);
+takeANumber(katzDeli, "Matz"); 
+currentLine(katzDeli); 
+nowServing(katzDeli);
+currentLine(katzDeli)
